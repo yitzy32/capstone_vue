46645cf31185db09326611508ae775fe2d57a78d
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-22 17:22:16 -0400

    add axios. replace Home.vue with template

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+import axios from "axios";
 
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 new Vue({
   router,
   render: h => h(App)
