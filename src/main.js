c313b6941f201b4c365ed810d4deceb319ec4296
Yitzy Bratt <yitzybratt32@gmail.com>
2021-05-12 21:42:24 -0400

    replece localhost with deployed frontend netlify site

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from "axios";
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://obscure-escarpment-34711.herokuapp.com" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
